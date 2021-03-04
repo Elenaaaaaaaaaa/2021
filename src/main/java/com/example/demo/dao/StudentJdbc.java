eec7882f47b89076733b92bd03768d601723d469
Senpai <>
2021-03-04 11:15:41 +0300

    it works ok

--- src/main/java/com/example/demo/dao/StudentJdbc.java
+++ src/main/java/com/example/demo/dao/StudentJdbc.java
@@ -57,13 +57,14 @@ public class StudentJdbc {
 
     }
 
-    public void UpdateStudent(Student student) {
+    public int UpdateStudent(Student student) {
         int status = jdbcTemplate.update("UPDATE STUDENT SET VALUES(?, ?, ?, ?)",
                 student.getId(), student.getSurname(),
                 student.getName(),student.getSecond_nameName());
         if(status == 0){
             CreateStudent(student);
         }
+        return status;
     }
 
 
